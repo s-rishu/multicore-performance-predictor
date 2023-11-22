e0843b28ff9aec9c6610b0c33aca26aa5453686f
meliodas137 <mayankgt1043@gmail.com>
2023-11-22 18:46:01 -0500

    added initial script for data generation

---
+++ datagen/datagen.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+simMemHardware() 
+{
+    for k in {1..2}
+    do
+        echo "Memory $k.ini configuration env..."
+        /home/mg7569/Multicore/project/multi2sim-5.0/bin/m2s --x86-sim detailed --mem-config ./mem-config/$k.ini --x86-config ./x86-config/$2.ini --ctx-config ./ctx-config/$1.ini
+        wait
+    done
+}
+
+simMultiHardware() 
+{
+    for j in {1..2}
+    do
+        echo "Hardware $j.ini configuration env...\n"
+        simMemHardware $1 $j
+    done
+}
+
+runMultiProg()
+{
+    for i in {1..2}
+    do
+        echo "Program $i benchmarking...\n"
+        simMultiHardware $i
+    done
+}
+
+runMultiProg
+
