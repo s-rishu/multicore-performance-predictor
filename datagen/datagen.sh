9b6462d1e11c04c6b38e8ee8554a1bf0ccd7941d
meliodas137 <mayankgt1043@gmail.com>
2023-11-23 22:34:52 -0500

    creating multiple temp files to automate threads input

--- datagen/datagen.sh
+++ datagen/datagen.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+ctx_dir=./ctx_config
+x86_dir=./x86_config
+mem_dir=./mem_config
+tmp=./tmp
+out=./out
+
 simMemHardware() 
 {
     for k in ./mem_config/*.ini
     do
         echo "Memory: $k configuration env..."
-        $m2s --x86-sim detailed --mem-config $k --x86-config $2 --ctx-config $1
+        $m2s --x86-sim detailed --x86-config $3 --ctx-config $tmp/$1 &>> ($out/$1)
         wait
     done
 }
@@ -15,22 +21,33 @@ simMultiHardware()
     for j in ./x86_config/*.ini
     do
         echo "Hardware: $j configuration env..."
-        simMemHardware $1 $j
+        simMemHardware $1 $2 $j
     done
 }
 
 runMultiProg()
 {
-    for i in ./ctx_config/*.ini
+    mkdir $tmp
+
+    configs=`ls $ctx_dir/* | xargs -n 1 basename`
+
+    for input in $configs
     do
-        echo "Program: $i benchmarking..."
-        for j in {1..16}
+        
+        echo "Program: $input benchmarking..."
+
+        for j in 1 2 4 8 16 32 64
         do
             echo "Threads count: $j total threads..."
-            sed -i "s|%NTHREADS|$j|g" $i
-            simMultiHardware $i
+            cp -rf $ctx_dir/$input $tmp/$input
+            sed -i "s|%NTHREADS|$j|g" $tmp/$input
+	        cat $tmp/$input
+            simMultiHardware $input
         done
     done
+
+    echo "Cleaning up temp files..."
+    rm -rf $tmp
 }
 
 runMultiProg
