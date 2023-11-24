cff4b9639fcb6ca26310c968089e1b7c6a7ce792
s-rishu <rishu.singh1203@gmail.com>
2023-11-23 22:07:26 -0500

    added script to generate config files

---
+++ configgen/gen_all_configs.py
@@ -0,0 +1,39 @@
+import itertools
+
+def read_ini_file(filename):
+    with open(filename, 'r') as f:
+        config = {}
+        for line in f:
+            if line.startswith('['):
+                section = line.strip('[]').strip()
+                config[section] = {}
+            elif '=' in line:
+                key, value = line.strip().split('=')
+                key = key.strip()
+                if value.startswith('[') and value.endswith(']'):
+                    value = value.strip('[]').strip().split(',')
+                config[section][key] = value
+    return config
+
+def generate_permutations(config):
+    for section, values in config.items():
+        for key, value in values.items():
+            if isinstance(value, list):
+                config[section][key] = itertools.permutations(value)
+    return config
+
+def print_permutations(config):
+    for section, values in config.items():
+        print(f'[{section}]')
+        for key, value in values.items():
+            if isinstance(value, itertools.permutations):
+                for permutation in value:
+                    print(f'{key} = {permutation}')
+            else:
+                print(f'{key} = {value}')
+
+if __name__ == '__main__':
+    filename = 'config.ini'
+    config = read_ini_file(filename)
+    config = generate_permutations(config)
+    print_permutations(config)
