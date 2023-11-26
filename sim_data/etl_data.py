6c90ca9d551b6970b0533ae468670cdda66dfd1a
meliodas137 <mayankgt1043@gmail.com>
2023-11-26 00:53:15 -0500

    reading the mem output for hitrates

--- sim_data/etl_data.py
+++ sim_data/etl_data.py
@@ -3,11 +3,10 @@ import os
 import pandas as pd
 
 # Global csv data
-global_columns = ["ProgId", "Cores", "Threads", "PageSize", "MemFrequency", "BlockSize", 
-                  "Latency", "L1_Sets", "L1_Assoc", "L1_BlockSize", "L1_Latency",
-                  "L2_Sets", "L2_Assoc", "L2_BlockSize", "L2_Latency",
-                  "L3_Sets", "L3_Assoc", "L3_BlockSize", "L3_Latency", 
-                  "Instructions", "InstructionsPerSecond", "SimTime", 
+global_columns = ["ProgId", "pThreads", "Mem_HitRate",
+                  "Mem_Latency", "L1_Sets", "L1_Latency", "L1_HitRate",
+                  "L2_Sets", "L2_Latency", "L2_HitRate", "L3_HitRate",
+                  "Instructions", "InstructionsPerSecond", "SimTime",
                   "Frequency", "Cycles", "CyclesPerSecond", "FastForwardInstructions",
                   "CommittedInstructions", "CommittedInstructionsPerCycle", "CommittedMicroInstructions", 
                   "CommittedMicroInstructionsPerCycle", "BranchPredictionAccuracy"]
@@ -17,15 +16,17 @@ global_data = []
 csvpath = './stats.csv'
 
 # input statistics data path
-inputPath = '../datagen/out/'
-x86Path = '../datagen/x86_config/'
-memPath = '../datagen/mem_config/'
+detailsPath = '../datagen/out/'
+memoutPath = '../datagen/memout/'
+sysoutPath = '../datagen/sysout/'
+x86Path = '../configgen/configs/x86_configs/'
+memPath = '../configgen/configs/mem_configs/'
 
 # section keys (and prefixes) to include from differnt configs to csv
 x86_sections = {"General": ""}
 out_sections = {"x86": ""}
-mem_sections = {"General": "Mem", "Module MainMemory": "", "CacheGeometry GeoNameL1": "L1_", 
-                        "CacheGeometry GeoNameL2": "L2_", "CacheGeometry GeoNameL3": "L3_"}
+mem_sections = {"Module MainMemory": "Mem_", "CacheGeometry GeoNameL1": "L1_", 
+                        "CacheGeometry GeoNameL2": "L2_"}
 
 
 def Merge(dict1, dict2, prefix = ""):
@@ -49,7 +50,7 @@ def MergeStats(outStats, x86Stats, memStats):
 
     return stats
  
-def read_ini_file(filename, count=False):
+def read_ini_file(filename):
     with open(filename, 'r') as f:
         stats = {}
         for line in f:
@@ -61,64 +62,75 @@ def read_ini_file(filename, count=False):
 
                 if section not in stats:
                     stats[section] = {}
-                if count:
-                    stats[section]["size"] = 0
     
             elif '=' in line:
                 key, value = line.strip().split('=')
                 key = key.strip()
-
-                if(key not in stats[section]):
-                    stats[section][key] = [] 
+ 
                 if value.startswith('[') and value.endswith(']'):
                     value = value.strip('[]').strip().split(',')
 
                 value = re.sub(r'\[.*?\]', '', value)
-                stats[section][key].append(value.strip())
-
-                if count:
-                    stats[section]["size"] = max(stats[section]["size"], len(stats[section][key]))
+                stats[section][key] = value.strip()
 
     return stats # Only returning x86 statistics
 
-def put_in_arr(stats, progId): 
-    for i in range(0, stats["size"]):
-        row = []
-        for key in global_columns:
-            if key == "ProgId":
-                row.append(progId)
-            elif key in stats:
-                if len(stats[key]) > 1:
-                    row.append(stats[key][i])
-                else:
-                    row.append(stats[key][0])
-            else:
-                row.append("")
+def get_hit_rate(memfile) :
+    hitRates = {"Mem_HitRate": 0, "L1_HitRate": 0, "L2_HitRate": 0, "L3_HitRate": 0}
+    memout = read_ini_file(memfile)
+    for key, val in memout.items():
+        if key == "MainMemory":
+            hitRates["Mem_HitRate"] = max(float(val["HitRatio"]), hitRates["Mem_HitRate"])
+        elif key == "CacheL3":
+            hitRates["L3_HitRate"] = max(float(val["HitRatio"]), hitRates["L3_HitRate"])
+        elif key.startswith("CacheL1"):
+            hitRates["L1_HitRate"] = max(float(val["HitRatio"]), hitRates["L1_HitRate"])
+        elif key.startswith("CacheL2"):
+            hitRates["L2_HitRate"] = max(float(val["HitRatio"]), hitRates["L2_HitRate"])
+    
+    for key, val in hitRates.items():
+        if val == 0:
+            hitRates[key] = 1
 
-        global_data.append(row)
+    return hitRates
+
+def put_in_arr(stats, progId, hitRates, pthreads): 
+    row = []
+    for key in global_columns:
+        if key == "ProgId":
+            row.append(progId)
+        elif key == "pThreads":
+            row.append(pthreads)
+        elif key in stats:
+            row.append(stats[key])
+        elif key in hitRates:
+            row.append(hitRates[key])
+        else:
+            row.append("")
+
+    global_data.append(row)
+        
 
 def dumpcsv():
     df = pd.DataFrame(global_data, columns=global_columns)
     df.to_csv(csvpath, index=False) # adding data in csv
 
 def readData():
-    for filename in os.listdir(inputPath):
+    for filename in os.listdir(detailsPath):
         
-        nums = re.findall(r'\d+', filename)
-        x86config = nums[len(nums)-2]
-        memconfig = nums[len(nums)-1]
-        progId = nums[0]
+        [progId, input, pthreads, cores, x86config, memconfig] = re.findall(r'\d+', filename)
 
-        file_path = os.path.join(inputPath, filename)
+        file_path = os.path.join(detailsPath, filename)
         if os.path.isfile(file_path):
 
-            stats = read_ini_file(file_path, True)
-            x86Stats = read_ini_file(x86Path + x86config + '.ini')
-            memStats = read_ini_file(memPath + memconfig + '.ini')
+            stats = read_ini_file(file_path)
+            x86Stats = read_ini_file(x86Path + cores + '/' + x86config + '.ini')
+            memStats = read_ini_file(memPath + cores + '/' + memconfig + '.ini')
 
+            hitRates = get_hit_rate(os.path.join(memoutPath, filename))
             stats = MergeStats(outStats=stats, x86Stats=x86Stats, memStats=memStats)
                     
-            put_in_arr(stats, progId)
+            put_in_arr(stats, progId, hitRates, pthreads)
         
     dumpcsv()
 
